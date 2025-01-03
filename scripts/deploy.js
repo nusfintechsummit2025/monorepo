a9a7fef6fd2db67da8f9265a9c58631868e8e763
Blankeeir <e1300538@u.nus.edu>
2025-01-03 09:07:01 +0800

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,10 +8,17 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   // Deploy RewardToken (ERC20)
-  const RewardToken = await hre.ethers.getContractFactory("RewardToken");
-  const rewardToken = await RewardToken.deploy("RewardToken", "RWT", 1000000);
-  await rewardToken.deployed();
-  console.log("RewardToken deployed to:", rewardToken.address);
+//   const RewardToken = await hre.ethers.getContractFactory("RewardToken");
+//   const rewardToken = await RewardToken.deploy("RewardToken", "RWT", 1000000);
+//   await rewardToken.deployed();
+//   console.log("RewardToken deployed to:", rewardToken.address);
+
+  // Deploy zkProofVerifier
+    const zkProofVerifier = await hre.ethers.getContractFactory("zkProofVerifier");
+    const zkProofVerifierInstance = await zkProofVerifier.deploy();
+    await zkProofVerifierInstance.deployed();
+    console.log("zkProofVerifier deployed to:", zkProofVerifierInstance.address);
+
 
   // Deploy PrivacyManager
   const PrivacyManager = await hre.ethers.getContractFactory("PrivacyManager");
