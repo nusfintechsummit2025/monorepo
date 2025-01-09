e09d12a775e565c71ed254434d0cc63ea0eb9b40
William-sqrx <william@sqrx.com>
2025-01-09 21:20:05 +0800

    add federated learning

--- backend/models/federated_learning.py
+++ backend/models/federated_learning.py
@@ -1,6 +1,122 @@
-# backend/federated_learning.py
+import collections
+import dp_accounting
+import numpy as np
+import pandas as pd
 import tensorflow as tf
 import tensorflow_federated as tff
 
-# Define model, data, and federated learning process
-# Implement training and aggregation
+def get_emnist_dataset():
+  emnist_train, emnist_test = tffg.simulation.datasets.emnist.load_data(
+      only_digits=True)
+  def element_fn(element):
+    return collections.OrderedDict(
+        x=tf.expand_dims(element['pixels'], -1), y=element['label'])
+  def preprocess_train_dataset(dataset):
+    return (dataset.map(element_fn)
+                   .shuffle(buffer_size=418)
+                   .repeat(1)
+                   .batch(32, drop_remainder=False))
+  def preprocess_test_dataset(dataset):
+    return dataset.map(element_fn).batch(128, drop_remainder=False)
+  emnist_train = emnist_train.preprocess(preprocess_train_dataset)
+  emnist_test = preprocess_test_dataset(
+      emnist_test.create_tf_dataset_from_all_clients())
+  return emnist_train, emnist_test
+train_data, test_data = get_emnist_dataset()
+def my_model_fn():
+  model = tf.keras.models.Sequential([
+      tf.keras.layers.Reshape(input_shape=(28, 28, 1), target_shape=(28 * 28,)),
+      tf.keras.layers.Dense(200, activation=tf.nn.relu),
+      tf.keras.layers.Dense(200, activation=tf.nn.relu),
+      tf.keras.layers.Dense(10)])
+  return tff.learning.models.from_keras_model(
+      keras_model=model,
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+      input_spec=test_data.element_spec,
+      metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+total_clients = len(train_data.client_ids)
+def train(rounds, noise_multiplier, clients_per_round, data_frame):
+  aggregation_factory = tff.learning.model_update_aggregator.dp_aggregator(
+      noise_multiplier, clients_per_round)
+  sampling_prob = clients_per_round / total_clients
+  learning_process = tff.learning.algorithms.build_unweighted_fed_avg(
+        my_model_fn,
+        client_optimizer_fn=tff.learning.optimizers.build_sgdm(0.01),
+        server_optimizer_fn=tff.learning.optimizers.build_sgdm(
+            1.0, momentum=0.9),
+        model_aggregator=aggregation_factory)
+  eval_process = tff.learning.build_federated_evaluation(my_model_fn)
+    
+  # Training loop.
+  state = learning_process.initialize()
+  for round in range(rounds):
+    if round % 5 == 0:
+      model_weights = learning_process.get_model_weights(state)
+      metrics = eval_process(model_weights, [test_data])['eval']
+      if round < 25 or round % 25 == 0:
+        print(f'Round {round:3d}: {metrics}')
+      data_frame = data_frame.append({'Round': round,
+                                      'NoiseMultiplier': noise_multiplier,
+                                      **metrics}, ignore_index=True)
+    x = np.random.uniform(size=total_clients)
+    sampled_clients = [
+        train_data.client_ids[i] for i in range(total_clients)
+        if x[i] < sampling_prob]
+    sampled_train_data = [
+        train_data.create_tf_dataset_for_client(client)
+        for client in sampled_clients]
+    # Use selected clients for update.
+    result = learning_process.next(state, sampled_train_data)
+    state = result.state
+    metrics = result.metrics
+  model_weights = learning_process.get_model_weights(state)
+  metrics = eval_process(model_weights, [test_data])['eval']
+  print(f'Round {rounds:3d}: {metrics}')
+  data_frame = data_frame.append({'Round': rounds,
+                                  'NoiseMultiplier': noise_multiplier,
+                                  **metrics}, ignore_index=True)
+  return data_frame
+data_frame = pd.DataFrame()
+rounds = 100
+clients_per_round = 50
+for noise_multiplier in [0.0, 0.5, 0.75, 1.0]:
+  print(f'Starting training with noise multiplier: {noise_multiplier}')
+  data_frame = train(rounds, noise_multiplier, clients_per_round, data_frame)
+  print()
+import matplotlib.pyplot as plt
+import seaborn as sns
+def make_plot(data_frame):
+  plt.figure(figsize=(15, 5))
+  dff = data_frame.rename(
+      columns={'sparse_categorical_accuracy': 'Accuracy', 'loss': 'Loss'})
+  plt.subplot(121)
+  sns.lineplot(data=dff, x='Round', y='Accuracy', hue='NoiseMultiplier', palette='dark')
+  plt.subplot(122)
+  sns.lineplot(data=dff, x='Round', y='Loss', hue='NoiseMultiplier', palette='dark')
+make_plot(data_frame)
+total_clients = 3383
+noise_to_clients_ratio = 0.01
+target_delta = 1e-5
+target_eps = 2
+make_fresh_accountant = dp_accounting.rdp.RdpAccountant
+def make_event_from_param(clients_per_round):
+  q = clients_per_round / total_clients
+  noise_multiplier = clients_per_round * noise_to_clients_ratio
+  gaussian_event = dp_accounting.GaussianDpEvent(noise_multiplier)
+  sampled_event = dp_accounting.PoissonSampledDpEvent(q, gaussian_event)
+  composed_event = dp_accounting.SelfComposedDpEvent(sampled_event, rounds)
+  return composed_event
+bracket_interval = dp_accounting.ExplicitBracketInterval(1, total_clients)
+clients_per_round = dp_accounting.calibrate_dp_mechanism(
+    make_fresh_accountant, make_event_from_param, target_eps, target_delta,
+    bracket_interval, discrete=True
+)
+noise_multiplier = clients_per_round * noise_to_clients_ratio
+print(f'To get ({target_eps}, {target_delta})-DP, use {clients_per_round} '
+      f'clients with noise multiplier {noise_multiplier}.')
+rounds = 100
+noise_multiplier = 1.2
+clients_per_round = 120
+data_frame = pd.DataFrame()
+data_frame = train(rounds, noise_multiplier, clients_per_round, data_frame)
+make_plot(data_frame)
